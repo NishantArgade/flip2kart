311d954b21c63b8832d0fc4650b756697e994c52
Nishant Argade <nishantargade4579@gmail.com>
2024-03-29 19:47:35 +0530

    leaflet  icon not show error fixed

--- client/src/modules/ContactUs/components/MapMarker.jsx
+++ client/src/modules/ContactUs/components/MapMarker.jsx
@@ -1,13 +1,26 @@
-/* eslint-disable react/prop-types */
-import { Marker, Popup, useMap } from "react-leaflet";
+import L from "leaflet"
+import { Marker, Popup, useMap } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import markerShadowPng from "leaflet/dist/images/marker-shadow.png"
+import { Icon } from "leaflet"
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIconPng,
+  iconUrl: markerIconPng,
+  shadowUrl: markerShadowPng,
+})
 
 const MapMarker = ({ city }) => {
-  const map = useMap();
+  const map = useMap()
   return (
     <Marker
+      icon={new Icon.Default()}
       eventHandlers={{
         click: () => {
-          map.flyTo([city.latitude, city.longitude], 15);
+          map.flyTo([city.latitude, city.longitude], 15)
         },
       }}
       position={[city.latitude, city.longitude]}
@@ -16,7 +29,7 @@ const MapMarker = ({ city }) => {
         <span>{city.name}</span>
       </Popup>
     </Marker>
-  );
-};
+  )
+}
 
-export default MapMarker;
+export default MapMarker
